37c7a361e4f890416fbbc8fdc236c3049970944a
ouxuyong <18289422015@163.com>
2023-04-09 16:17:16 +0800

    提交模板方法模式代码

---
+++ oxy-education-web/src/test/java/com/oxygen/education/designmode/template/ordinary/OrdinaryTest.java
@@ -0,0 +1,21 @@
+package com.oxygen.education.designmode.template.ordinary;
+
+import org.junit.Test;
+
+public class OrdinaryTest {
+
+    @Test
+    public void test(){
+        Programmer programmer = new Programmer();
+        programmer.wakeUp();
+        programmer.behavior();
+        programmer.sleep();
+
+        System.out.println("============================================================");
+
+        Doctor doctor = new Doctor();
+        doctor.wakeUp();
+        doctor.behavior();
+        doctor.sleep();
+    }
+}
